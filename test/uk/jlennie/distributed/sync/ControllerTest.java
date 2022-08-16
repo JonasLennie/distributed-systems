00ea4dc098e53d64ddef3614e5697115de1bd416
JonasLennie <contact@jlennie.uk>
2022-08-16 08:54:57 +0100

    Commit With ControllerTest template

--- test/uk/jlennie/distributed/sync/ControllerTest.java
+++ test/uk/jlennie/distributed/sync/ControllerTest.java
@@ -1,11 +1,48 @@
 package uk.jlennie.distributed.sync;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
     // Bad PIDs
     // Bad Connections
     // Run behaviour same
+    @Test
+    void testSingleNodeNoConnections() {
 
+    }
 
+    @Test
+    void testTwoNodesOneConnectionTerminates() {
+
+    }
+
+    @Test
+    void testTwoNodesBiDirectionalTerminates() {
+
+    }
+
+    @Test
+    void testLargeNetworkCommunicatesTerminates() {
+
+    }
+
+    @Test
+    void testPIDsNotUnique() {
+
+    }
+
+    @Test
+    void testBadPIDsForConnection() {
+
+    }
+
+    @Test
+    void testExecuteIdenticalToExecutorTest() {
+
+    }
+
+    // Unique PIDs in input list
+    // Bad PID in connection
 }
